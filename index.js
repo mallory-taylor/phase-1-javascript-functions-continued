e70859c288cdce93e947242b236f972fdd042999
Mallory Taylor <malloryataylor13@gmail.com>
2023-03-21 15:11:08 -0400

    Declared functions with default values

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+function wrapAdjective(flair = "*") {
+    return function adjective (adj = "special") {
+        return `You are ${flair}${adj}${flair}!`;
+    };
+}
